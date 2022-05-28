8405173e3832243e24bc026cc12379699041a00a
adriandamiao <adriandami_@hotmail.com>
2022-05-28 19:23:43 -0300

    Corrige a validação de preço

--- src/js/validation.js
+++ src/js/validation.js
@@ -4,4 +4,5 @@
         delimiter: '.'});
     VMasker(document.getElementById("inputAno")).maskNumber();
     VMasker(document.getElementById("inputAno")).maskPattern('9999');
+    VMasker(document.getElementById("inputPreco")).maskPattern('99.999.999,99');
 })();
