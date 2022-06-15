7d0b3e696c5630ac384ecbe4a5c7e23a5286011f
Luan <j0hn04v@gmail.com>
2022-06-15 12:18:42 -0300

    Add files via upload

--- src/js/validation.js
+++ src/js/validation.js
@@ -1,6 +1,9 @@
 (function(){
     VMasker(document.getElementById("inputPreco")).maskMoney({
+        precision: 2,
         separator: ',',
+        zeroCents: true,
+        unit: 'R$',
         delimiter: '.'});
     VMasker(document.getElementById("inputAno")).maskNumber();
     VMasker(document.getElementById("inputAno")).maskPattern('9999');
